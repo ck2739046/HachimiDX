98fbf8da6e752116fd5b64ee9e2212b91342ec22
simon <49937999+ck2739046@users.noreply.github.com>
2026-09-05 22:15:30 +0800

    [-] app.majdata_page: 选择 "---" 不再 reset majdata

--- src/app/pages/majdata_page.py
+++ src/app/pages/majdata_page.py
@@ -169,14 +169,13 @@ class MajdataPage(QWidget):
                      and f.lower() not in ("track_result.txt", "detect_result.txt", "note_preprocess_result.txt")]
         if txt_files:
             txt_files = sorted(txt_files)
-        txt_files.insert(0, "---")  # 添加默认空选项
         self._maidata_combo.addItems(txt_files)
         # 如果有 maidata.txt，优先选择它
         if "maidata.txt" in txt_files:
             index = txt_files.index("maidata.txt")
             self._maidata_combo.setCurrentIndex(index)
         # 否则默认选择第一个
-        else:
+        elif txt_files:
             self._maidata_combo.setCurrentIndex(0)
 
         # scan mp3/ogg and add to track_combo
@@ -215,7 +214,6 @@ class MajdataPage(QWidget):
 
         # Check if song directory is selected
         if self._selected_song_path is None:
-            self.reset_majdataview()
             return
         
         selected_maidata = self._maidata_combo.currentText()
@@ -223,8 +221,7 @@ class MajdataPage(QWidget):
         selected_video = self._video_combo.currentText()
         is_play_video = self._play_video_checkbox.isChecked()
 
-        if not selected_maidata or not selected_track or selected_maidata == "---":
-            self.reset_majdataview()
+        if not selected_maidata or not selected_track:
             return
 
         majdataview_has_video = bool(selected_video and is_play_video)
