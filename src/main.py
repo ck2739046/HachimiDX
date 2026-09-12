e614acec69ac0c4d930945d859343f9ae08493c4
simon <49937999+ck2739046@users.noreply.github.com>
2026-09-12 23:27:37 +0800

    bump version

--- src/main.py
+++ src/main.py
@@ -66,7 +66,7 @@ from src.services import AllServices
 author = "ck2739046"
 repo_name = "HachimiDX"
 
-VERSION = "1.6.2"
+VERSION = "1.6.3"
 REPO = f"https://github.com/{author}/{repo_name}"
 API_RELEASE_LATEST = f"https://api.github.com/repos/{author}/{repo_name}/releases/latest"
 
